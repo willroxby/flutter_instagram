51ee4c0ae767f5f2e85e6a57b2c6b557093f2634
Will Roxby <34317518+TheBolly@users.noreply.github.com>
2021-10-09 17:40:57 +0100

    Updated to flutter 2.0

--- lib/src/models/refresh_access_token.dart
+++ lib/src/models/refresh_access_token.dart
@@ -6,9 +6,9 @@ class RefreshAccessToken extends Object {
 
   factory RefreshAccessToken.fromJson(Map<String, dynamic> json) => _$RefreshAccessTokenFromJson(json);
 
-  String access_token;
-  String token_type;
-  int expires_in;
+  String? access_token;
+  String? token_type;
+  int? expires_in;
 
   @override
   String toString() => '$runtimeType { access_token: $access_token, token_type: $token_type, expires_in: $expires_in}';
